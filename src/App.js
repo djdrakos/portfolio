776888770a8a301574b16ae4a85b5fe45aeddb82
DJ Drakos <dianajodrakos@gmail.com>
2022-10-20 11:53:02 -0700

    add basic scroll styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
+// import './djd_icon.svg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World.
-    </div>
+    <>
+      <header>
+        <img src="./djd_icon.svg" alt="DJD icon" />
+      </header>
+      <main className="App">
+        <section></section>
+          <button className="toggle-mode">
+            <img src="./sun.png" alt="sun icon" width="40px" />
+          </button>
+        <section className="projects">
+          <h2>
+          Projects
+          </h2>
+        </section>
+        <section className="experience">
+          <h2>
+            Experience
+          </h2>
+        </section>
+        <section className="contact">
+          <h2>
+          Contact
+          </h2>
+        </section>
+        <section className="about">
+          <h2>
+          About
+          </h2>
+        </section>
+        <section></section>
+      </main>
+    </>
   );
 }
 
