dfcfc1051f383fff7f87c669216bb846c279b099
DJ Drakos <dianajodrakos@gmail.com>
2022-10-31 16:07:04 -0700

    add camelCase formatting helper function

--- src/helpers/formatting-helpers.js
+++ src/helpers/formatting-helpers.js
@@ -1,5 +1,9 @@
-import { paramCase } from 'change-case'
+import { paramCase, camelCase } from 'change-case'
 
 export function formatClassName(string) {
   return paramCase(string)
+}
+
+export function formatCamelCase(string) {
+  return camelCase(string)
 }
