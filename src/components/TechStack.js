31758e884505dff5bfa0e2f3e8b91b9a8af565ac
DJ Drakos <dianajodrakos@gmail.com>
2022-10-31 16:07:27 -0700

    change title prop

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -27,7 +27,7 @@ const StyledTechStack = styled(SectionContent)`
 
 const TechStack = forwardRef((props, ref) => {
   return (
-    <StyledTechStack ref={ref} title="Tech Stack" {...props}>
+    <StyledTechStack ref={ref} title="tech-stack" {...props}>
       <h5>Lorem Ipsum</h5>
       <p>dolor sit amet, consectetur adipiscing elit. Phasellus porta et diam in vestibulum. Aliquam nunc enim, condimentum in volutpat viverra, ultricies non sem. Phasellus eu nisi mauris. Nunc at molestie enim. Curabitur finibus suscipit felis ut congue. Phasellus sed justo velit. Aenean neque purus, fermentum sit amet scelerisque tempus, ultrices eget massa. Etiam eget ante augue.</p>
       <p>Vivamus eu orci id nisl cursus fermentum in sed elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi efficitur nunc a quam faucibus egestas. Pellentesque viverra arcu eu suscipit sollicitudin.</p>
