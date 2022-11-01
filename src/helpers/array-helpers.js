ae9a7e9f174560942664505860dd15ad9b5e7e08
DJ Drakos <dianajodrakos@gmail.com>
2022-11-01 00:11:10 -0700

    bugfix: this is why you should be writing tests

--- src/helpers/array-helpers.js
+++ src/helpers/array-helpers.js
@@ -1,11 +1,10 @@
 export function generateThresholds(steps) {
   let thresholds = []
-  thresholds.push(0)
-  for (let i = 1; i >= steps; i++) {
+    thresholds.push(0)
+  for(let i = 1; i <= steps; i++) {
     const factor = 10 ** 2
     const num = i / steps
     thresholds.push(Math.round(num * factor) / factor)
   }
-
   return thresholds
 }
