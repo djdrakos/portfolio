56afe48cce31efd6e0f194354a79c43c151a8786
DJ Drakos <dianajodrakos@gmail.com>
2022-11-20 19:40:05 -0800

    fix portrait resize

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,32 +4,39 @@ import GridContainer from './GridContainer'
 import ThoughtSpiral from './ThoughtSpiral'
 import * as Tooltip from '@radix-ui/react-tooltip'
 import GridItem from './GridItem'
+import breakpoints from '../styles/breakpoints'
 
 const StyledHero = styled(GridContainer)`
   height: min-content;
 
-  .wrapper.portrait {
-    max-width: var(--bio-pic100);
+  .wrapper-portrait {
+    width: 15rem;
+    margin-left: auto;
     aspect-ratio: 1;
     border-radius: 50%;
     background-color: ${({ theme }) => theme.type === 'dark' ? theme.color300 : 'none'};
     overflow: hidden;
   } 
 
-  img.portrait {
+  .portrait {
     isolation: isolate;
-    max-width: 130%;
     height: auto;
     filter: grayscale();
     mix-blend-mode: ${({ theme }) => theme.type === 'dark' ? 'overlay' : ''};
     object-fit: contain;
-    object-position: calc( -1 * var(--gap100)) calc( -1 * var(--gap150));
     opacity: ${({ theme }) => theme.type === 'dark' ? .5 : .6 };
     }
     
   .fixed {
     position: fixed;
     top: 0;
+  }
+
+  @media screen and (${breakpoints.tablet}) {
+    .wrapper-portrait{
+      max-width: 12.75rem;
+      width: 100%;
+    }
   }
   `
 
@@ -38,8 +45,8 @@ export default function Hero({ toggleTheme }) {
   return (
     <StyledHero>
       <Tooltip.Provider delayDuration={100}>
-        <GridItem m={3} justifyContent={'flex-end'}>
-          <div className="wrapper portrait">
+        <GridItem m={3}>
+          <div className="wrapper-portrait">
             <img src={require("../assets/dj.jpg")} className="portrait" alt="DJ Drakos sitting on a couch, staring intently at something on their laptop" />
           </div>
           <ThoughtSpiral/>
