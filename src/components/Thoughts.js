3d522c66f528e624ab15a052a4eef8b9b36a7e96
DJ Drakos <d.josi.drakos@gmail.com>
2023-02-25 20:41:07 -0800

    Improve random thought logic

---
+++ src/components/Thoughts.js
@@ -0,0 +1,13 @@
+import { shuffleArray } from '../helpers/array-helpers';
+
+const thoughts = [
+  'My other website is on Geocities',
+  'Coding... Coding...',
+  'Tea time: 3pm, every day',
+  'Rome wasn\'t built in a day, but Javascript was',
+  'Looks like good coding weather today'
+]
+
+const randomThoughts = shuffleArray(thoughts)
+
+export default randomThoughts
--- src/helpers/array-helpers.js
+++ src/helpers/array-helpers.js
@@ -1,10 +1,26 @@
 export function generateThresholds(steps) {
   let thresholds = []
     thresholds.push(0)
+
   for(let i = 1; i <= steps; i++) {
     const factor = 10 ** 2
     const num = i / steps
     thresholds.push(Math.round(num * factor) / factor)
   }
+
   return thresholds
+}
+
+export function shuffleArray(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  
+  return array;
 }
